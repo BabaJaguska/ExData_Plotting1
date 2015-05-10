ac9d5e3c276e345ce5237f9fdd07a6fde56a8e3c
BabaJaguska <minja.n.belic@gmail.com>
2015-05-10 16:56:26 +0200

    Fixed a mistake

--- !R codes and plots/plot1.R
+++ !R codes and plots/plot1.R
@@ -1,6 +1,7 @@
 setwd("C:/Users/Belici/Desktop/mama/COURSERA/explor data/Assign1/")
-a<-read.table("household_power_consumption.txt",header=TRUE,na.strings="?",nrows=2100000,colClasses="character")
-a$Date<-as.Date(a$Date)
-Data<-a[which(a$Date=="01-02-2007"|a$Date=="02-02-2007"),]
+a<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",nrows=2100000,colClasses="character")
+a$Date<-as.Date(a$Date,format="%d/%m/%Y")
+Data<-a[which((a$Date=="2007-02-01")|(a$Date=="2007-02-02")),]
 Data$Global_active_power<-as.numeric(Data$Global_active_power)
 hist(Data$Global_active_power,col="red",xlab="Global Active Power (kilowats)",main="Global Active Power")
+png("plot1.png")
